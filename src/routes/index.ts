cf8189a4cf48fac3a22500464415b4e34970264c
Francisco Oliveira <74855391+franciscomgoliveira@users.noreply.github.com>
2022-04-12 12:24:02 +0100

    commit

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,7 @@ import inviteRouter from './invite';
 import insightsRouter from './insights';
 import manageRouter from './manage';
 import votingRouter from './voting';
+import botRouter from './bot';
 
 const router = Router();
 
@@ -14,5 +15,6 @@ router.use('/invite', inviteRouter);
 router.use('/insights', insightsRouter);
 router.use('/manage', manageRouter);
 router.use('/voting', votingRouter);
+router.use('/bot', botRouter)
 
 export default router;
